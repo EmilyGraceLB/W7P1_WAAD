999a09aa1636f6d1552eca52500addc190e28f14
Emily <145925732+EmilyGraceLB@users.noreply.github.com>
2025-03-26 21:26:39 +0000

    task 2

--- STAGE2/stage2.js
+++ STAGE2/stage2.js
@@ -4,8 +4,25 @@ function theme(backgroundColour,textColour,font) {
     this.font = font;
 }
 
+//created 3 new objects for different themes
 let theme1 = new theme('#ff5733', '#93ff93', 'Verdana');
 let theme2 = new theme('#ff93d1', '#93e1ff', 'Roboto');
 let theme3 = new theme('#ffee93', '#e193ff', 'Helvetica Neue');
 
-console.log(theme1, theme2, theme3);
+// console.log(theme1, theme2, theme3);
+//created three different variables to manage the buttons 
+let b1 = document.getElementById("t1");
+let b2 = document.getElementById("t2");
+let b3 = document.getElementById("t3");
+
+if(b1) {
+    theme1;
+}
+
+if(b2) {
+    theme2;
+}
+
+if(b3){
+    theme3;
+}
