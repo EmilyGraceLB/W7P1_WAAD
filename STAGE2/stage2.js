962f768f32d22046a44c82ec1b4446f9fc4b7082
Emily <145925732+EmilyGraceLB@users.noreply.github.com>
2025-03-28 14:26:51 +0000

    completed p1

--- STAGE2/stage2.js
+++ STAGE2/stage2.js
@@ -1,3 +1,4 @@
+//creates objects for themes
 function theme(backgroundColour,textColour,font) {
     this.backgroundColour = backgroundColour;
     this.textColour = textColour;
@@ -6,23 +7,30 @@ function theme(backgroundColour,textColour,font) {
 
 //created 3 new objects for different themes
 let theme1 = new theme('#ff5733', '#93ff93', 'Verdana');
-let theme2 = new theme('#ff93d1', '#93e1ff', 'Roboto');
-let theme3 = new theme('#ffee93', '#e193ff', 'Helvetica Neue');
+let theme2 = new theme('#ff93d1', '#93e1ff', '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif');
+let theme3 = new theme('#ffee93', '#e193ff', '"Courier New", Courier, monospace');
 
-// console.log(theme1, theme2, theme3);
 //created three different variables to manage the buttons 
-let b1 = document.getElementById("t1");
-let b2 = document.getElementById("t2");
-let b3 = document.getElementById("t3");
+// let b1 = document.getElementById("t1");
+// let b2 = document.getElementById("t2");
+// let b3 = document.getElementById("t3");
 
-if(b1) {
-    theme1;
+//pair themes to buttons
+function pairTheme(theme, element) {
+    element.style.backgroundColor = theme.backgroundColour;
+    element.style.color = theme.textColour;
+    element.style.fontFamily = theme.font;
 }
 
-if(b2) {
-    theme2;
-}
+//paring themes to labels
+pairTheme(theme3, document.getElementById("t3"));
+pairTheme(theme2, document.getElementById("t2"));
+pairTheme(theme1, document.getElementById("t1"));
 
-if(b3){
-    theme3;
+for(let btn of document.getElementsByName("theme")) {
+    btn.addEventListener("change", function (event) {
+        const selected = event.target.value;
+        const theme = selected === "op1" ? theme1 : selected === "op2" ? theme2 : theme3;
+        pairTheme(theme, document.body);
+    })
 }
